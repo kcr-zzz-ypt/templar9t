60b5920897c72b3e84e67a67d1de077809d1f5a3
kcrypt <jenkinsdon99@gmail.com>
2019-11-01 15:12:41 +0100

    my first commit

---
+++ javascript/scriptz.js
@@ -0,0 +1,19 @@
+
+// var name = "cipher";
+// var location = "Akinrele str";
+// var age = 100;
+// console.log("my name is" +name+ "I live at" +location+ "I am" +age+ "years old"); 
+// let name = "cipher";
+// let age = 30;
+// document.write("my name is" +name);
+
+
+
+num1 = "3";
+num2 = 3;
+
+console.log(num1 + num2);
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(num1 && num2);
+console.log(num1 || num2);
